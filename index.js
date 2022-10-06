6213af68f286462d6ff977f3b1b7957f8e243f71
Anslie <brantae@gmail.com>
2022-10-05 21:26:00 -0500

    improved functions readability and refactored

--- index.js
+++ index.js
@@ -7,9 +7,15 @@ let form = document.querySelector("#form-abv")
 const beerForm = document.querySelector('form')
 //console.log(beerForm)
 
+
 getAllBeers()
 formEventListener()
 addNewBeer()
+
+
+/*
+likeList.addEventListener('click', addLikedToList(beer))
+*/
 
 function getAllBeers() {
   return fetch(url)
@@ -22,9 +28,14 @@ function getAllBeers() {
       
      })
      
+     
  })
- .then(addLikeButtons);
+ .then(addLikeButton)
+ //.then(addLikedToList)
+ 
+
 }
+
 
 
 //Tells user to input a valid whole number
@@ -51,28 +62,6 @@ function addNewBeer() {
   })
 }
 
-//Adds like buttons to each beer card
-function addLikeButtons() {
-  
-  let button = document.querySelectorAll('.try')
-  //console.log(button)
-    button.forEach((btn) =>
-   {
-    
-    btn.addEventListener('click', (e) => {
-    console.log('click')
-    console.log(e)
-    console.log(btn)
-    updateList(name, description, abv)
-
-
-    
-    })
-  })
-
-  
-}
-
 
 //Renders each beer card from API
 function renderBeer(beer) {
@@ -81,20 +70,56 @@ function renderBeer(beer) {
   <p> ${beer.tagline} <br>
   <p> ${beer.abv}% ABV </p>
   </p></h2>
-  <img src=${beer.image_url} class='beer-pic' />
-  <footer>
-  <ul>
-  <li> Favorite Me!
-  <button class='try'> ${heart} </button>
-  </li>
-  </ul>
-  </footer>
-  </div>
-  `
+  <img src=${beer.image_url} class='beer-pic' /> `
+  
   container.innerHTML += beerCard;
 }
 
-//Adds a beer from the cards into the list of liked beers when heart is clicked
+//Adds a like button to each card
+function addLikeButton() {
+  let cards = document.querySelectorAll('.card')
+  //console.log(cards)
+
+
+  cards.forEach(card => {
+  let footer = document.createElement('footer')
+  let ul = document.createElement('ul')
+  let li = document.createElement('li')
+  let button = document.createElement('button')
+  button.className = 'heart'
+  button.innerText = `Add me to the list! ${heart}`
+
+
+  //console.log(card)
+  card.append(footer)
+  footer.appendChild(ul)
+  ul.appendChild(li)
+  li.append(button)
+
+  card.addEventListener('click', (e) => {
+    console.log('click')
+    console.log(e)
+    addLikedToList()
+  })
+  })
+
+   
+  
+}
+
+//Functionality of the like button
+//Adds the beer that is liked to the list of 'Beers I like'
+function addLikedToList(beer) {
+  let p = document.createElement('p');
+  p.className = 'liked-beer'
+  p.innerHTML = `•${beer.name} <br> •${beer.description} <br> •${beer.abv}%`
+  likeList.appendChild(p)
+ 
+}
+
+
+
+//Adds a beer into the list when inputs are submitted in the form
   function updateList(name, description, abv) {
     
     let p = document.createElement('p');
@@ -104,3 +129,4 @@ function renderBeer(beer) {
     
   }
 
+  
